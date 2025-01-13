dmzz-yyhyy/LightNovleReader

优化翻页效果 #136

app/src/main/kotlin/indi/dmzz_yyhyy/lightnovelreader/ui/home/settings/data/MenuOptions.kt
@@ -1,7 +1,12 @@
 package indi.dmzz_yyhyy.lightnovelreader.ui.home.settings.data
 
 sealed class MenuOptions(vararg options: Options) {
-    val optionsList: List<Options> = options.toList()
+    private val _optionsList: MutableList<Options> = options.toMutableList()
+    val optionsList: List<Options> get() = _optionsList.toList()
+    fun option(key: String, name: String): String {
+        _optionsList.add(Options(key, name))
+        return key
+    }
     class Options(
         val key: String,
         val name: String
@@ -29,4 +34,9 @@ sealed class MenuOptions(vararg options: Options) {
         Options("zh-TW", "zh_TW"),
         Options("ja-JP", "ja_JP")
     )
+
+    data object FlipAnimeOptions: MenuOptions() {
+        val None = option("none", "无")
+        val ScrollWithoutShadow = option("scroll", "滚动")
+    }
 }

Lain4504/novel-reading-mobile

app/src/main/kotlin/indi/dmzz_yyhyy/lightnovelreader/ui/home/settings/data/MenuOptions.kt
@@ -1,7 +1,12 @@
 package indi.dmzz_yyhyy.lightnovelreader.ui.home.settings.data
 
 sealed class MenuOptions(vararg options: Option) {
-    val optionList: List<Option> = options.toList()
+    private val _optionsList: MutableList<Option> = options.toMutableList()
+    val optionsList: List<Option> get() = _optionsList.toList()
+    fun option(key: String, name: String): String {
+        _optionsList.add(Option(key, name))
+        return key
+    }
     class Option(
         val key: String,
         val name: String
@@ -9,7 +14,7 @@ sealed class MenuOptions(vararg options: Option) {
         override fun equals(other: Any?): Boolean = this.key == other
         override fun hashCode(): Int = key.hashCode()
     }
-    fun get(key: String): Option = optionList.first { it.equals(key) }
+    fun get(key: String): Option = _optionsList.first { it.equals(key) }
 
     data object UpdateChannelOptions: MenuOptions(
         Option("Release", "正式发布版"),
@@ -37,5 +42,8 @@ sealed class MenuOptions(vararg options: Option) {
         Option("ko-kp", "한국어 (조선민주주의인민공화국)")
     )
 
-
+    data object FlipAnimeOptions: MenuOptions() {
+        val None = option("none", "无")
+        val ScrollWithoutShadow = option("scroll", "滚动")
+    }
 }