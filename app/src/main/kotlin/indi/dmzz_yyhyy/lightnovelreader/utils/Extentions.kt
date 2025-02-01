56eefc691e05d147c95c7b69f738599f4d3bf86e
yukonisen <whenten99@gmail.com>
2025-02-01 16:01:13 +0800

    优化详情页UI和动画 新增书本详情信息 优化隐藏已读章节提示

--- app/src/main/kotlin/indi/dmzz_yyhyy/lightnovelreader/utils/Extentions.kt
+++ app/src/main/kotlin/indi/dmzz_yyhyy/lightnovelreader/utils/Extentions.kt
@@ -1,6 +1,10 @@
 package indi.dmzz_yyhyy.lightnovelreader.utils
 
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.produceState
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.graphics.BlendMode
@@ -27,3 +31,21 @@ fun Modifier.fadingEdge(brush: Brush) = this
         drawContent()
         drawRect(brush = brush, blendMode = BlendMode.DstIn)
     }
+
+@Composable
+fun LazyListState.isScrollingUp(): State<Boolean> {
+    return produceState(initialValue = true) {
+        var lastIndex = 0
+        var lastScroll = Int.MAX_VALUE
+        snapshotFlow {
+            firstVisibleItemIndex to firstVisibleItemScrollOffset
+        }.collect { (currentIndex, currentScroll) ->
+            if (currentIndex != lastIndex || currentScroll != lastScroll) {
+                value = currentIndex < lastIndex ||
+                        (currentIndex == lastIndex && currentScroll < lastScroll)
+                lastIndex = currentIndex
+                lastScroll = currentScroll
+            }
+        }
+    }
+}
