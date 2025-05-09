1296eae8b741d4ff29eb4c04e0480bc9b917a785
yukonisen <whenten99@gmail.com>
2025-05-09 23:02:51 +0800

    更新版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         minSdk = 24
         targetSdk = 35
         // 版本号为x.y.z则versionCode为x*1000000+y*10000+z*1000+debug版本号(开发需要时迭代, 三位数)
-        versionCode = 1_01_00_020
+        versionCode = 1_01_00_021
         versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
