261fa9822a1d64cd1469ef29bf5d0b00a389a80d
yukonisen#QwQ <whenten99@gmail.com>
2025-08-01 16:05:38 +0800

    修复图片加载指示器

--- app/src/main/kotlin/indi/dmzz_yyhyy/lightnovelreader/ui/components/ZoomableAsyncImage.kt
+++ app/src/main/kotlin/indi/dmzz_yyhyy/lightnovelreader/ui/components/ZoomableAsyncImage.kt
@@ -4,12 +4,16 @@ import androidx.compose.foundation.gestures.calculateZoom
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.PointerEventPass
@@ -19,6 +23,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
 import coil.compose.AsyncImage
+import coil.compose.AsyncImagePainter
 import coil.request.ImageRequest
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -33,6 +38,7 @@ fun ZoomableImage(
 ) {
     val coroutineScope = rememberCoroutineScope()
     var scale by remember { mutableFloatStateOf(1f) }
+    var isLoading by remember { mutableStateOf(true) }
 
     Box(
         modifier = modifier
@@ -73,16 +79,34 @@ fun ZoomableImage(
                 scaleY = scale
             }
     ) {
+        if (isLoading) {
+            CircularProgressIndicator(
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .padding(vertical = 64.dp),
+                color = MaterialTheme.colorScheme.primary
+            )
+        }
+
         AsyncImage(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(bottom = 8.dp),
+                .padding(bottom = 8.dp)
+                .align(Alignment.Center),
             model = ImageRequest.Builder(LocalContext.current)
                 .data(imageUrl)
                 .crossfade(true)
                 .build(),
             contentScale = ContentScale.FillWidth,
-            contentDescription = null
+            contentDescription = null,
+            onState = { state ->
+                if (state is AsyncImagePainter.State.Success
+                    || state is AsyncImagePainter.State.Error
+                ) {
+                    isLoading = false
+                }
+            }
         )
     }
 }
+
