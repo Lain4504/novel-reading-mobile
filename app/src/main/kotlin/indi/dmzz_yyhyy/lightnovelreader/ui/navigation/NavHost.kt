b49d17ca6aa979cd748a679a90270e98f1b50506
yukonisen#QwQ <whenten99@gmail.com>
2025-08-29 23:29:51 +0800

    补全文件

---
+++ app/src/main/kotlin/indi/dmzz_yyhyy/lightnovelreader/ui/navigation/NavHost.kt
@@ -0,0 +1,52 @@
+package indi.dmzz_yyhyy.lightnovelreader.ui.navigation
+
+import androidx.compose.animation.ExperimentalSharedTransitionApi
+import androidx.compose.animation.SharedTransitionLayout
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.remember
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import indi.dmzz_yyhyy.lightnovelreader.ui.LocalNavController
+import indi.dmzz_yyhyy.lightnovelreader.ui.book.bookNavigation
+import indi.dmzz_yyhyy.lightnovelreader.ui.dialog.addBookToBookshelfDialog
+import indi.dmzz_yyhyy.lightnovelreader.ui.dialog.markAllChaptersAsReadDialog
+import indi.dmzz_yyhyy.lightnovelreader.ui.dialog.updatesAvailableDialog
+import indi.dmzz_yyhyy.lightnovelreader.ui.downloadmanager.downloadManager
+import indi.dmzz_yyhyy.lightnovelreader.ui.home.homeNavigation
+import indi.dmzz_yyhyy.lightnovelreader.utils.LocalSnackbarHost
+import indi.dmzz_yyhyy.lightnovelreader.utils.expandEnter
+import indi.dmzz_yyhyy.lightnovelreader.utils.expandExit
+import indi.dmzz_yyhyy.lightnovelreader.utils.expandPopEnter
+import indi.dmzz_yyhyy.lightnovelreader.utils.expandPopExit
+
+@OptIn(ExperimentalSharedTransitionApi::class)
+@Composable
+fun LightNovelReaderNavHost(
+    navController: NavHostController
+) {
+    val snackbarHostState = remember { SnackbarHostState() }
+    CompositionLocalProvider(
+        LocalNavController provides navController,
+        LocalSnackbarHost provides snackbarHostState
+    ) {
+        SharedTransitionLayout {
+            NavHost(
+                navController = navController,
+                startDestination = Route.Main,
+                enterTransition = { expandEnter() },
+                exitTransition = { expandExit() },
+                popEnterTransition = { expandPopEnter() },
+                popExitTransition = { expandPopExit() }
+            ) {
+                homeNavigation(this@SharedTransitionLayout)
+                bookNavigation()
+                updatesAvailableDialog()
+                addBookToBookshelfDialog()
+                downloadManager()
+                markAllChaptersAsReadDialog()
+            }
+        }
+    }
+}
